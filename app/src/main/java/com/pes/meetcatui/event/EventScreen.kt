42c3a1f2d8592a942541a78d10469881f7f9370c
Yavanna92 <txellguevara.1905@gmail.com>
2022-10-15 17:52:03 +0200

    Event view with hardcoded data and no navigation

--- app/src/main/java/com/pes/meetcatui/event/EventScreen.kt
+++ app/src/main/java/com/pes/meetcatui/event/EventScreen.kt
@@ -1,135 +1,127 @@
 package com.pes.meetcatui.event
 
-import android.graphics.Paint.Align
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.scrollable
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.pes.meetcatui.ui.theme.MeetCatUITheme
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.ui.Alignment.Companion.Start
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import com.pes.meetcatui.R
+import com.pes.meetcatui.common.composables.BackButton
+import com.pes.meetcatui.common.composables.SpaceDp
 
 @Composable
-fun EventTitle(name: String) {
-    Row (Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-        Text(
-            text = "Hello $name!",
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            fontSize = 26.sp,
-        )
-    }
-}
-
-@Composable
-fun EventSubtitle(name: String) {
+fun EventDate(startDate: String = "[ ]", endDate: String = "[ ]") {
     Text(
-        text = "Hello $name!",
-        fontWeight = FontWeight.Bold,
-        fontSize = 18.sp,
-    )
-}
-
-@Composable
-fun EventDate(startDate: String?, endDate: String?) {
-    val strStartDate = if(startDate.isNullOrEmpty()) "[ ]" else startDate
-    val strEndDate = if(endDate.isNullOrEmpty()) "[ ]" else endDate
-    Text(
-        text = "$strStartDate - $strEndDate",
-        fontWeight = FontWeight.Bold,
-        fontSize = 18.sp
+        text = "$startDate - $endDate",
+        style = MaterialTheme.typography.h4,
     )
 }
 
 @Composable
 fun EventDetailsItem(name: String, content: String) {
     Text(text = name,
-        fontSize = 16.sp,
+        fontSize = 18.sp,
         fontWeight = FontWeight.Bold
     )
-    Spacer(modifier = Modifier.padding(3.dp))
-    Text(text = content,
-        fontSize = 14.sp
+    SpaceDp(4)
+    Text(
+        text = content,
+        style = MaterialTheme.typography.body1,
+        textAlign = TextAlign.Justify
+    )
+}
+
+@Composable
+fun EventDetailsLinkItem(name: String, url: String) {
+    Text(text = name,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold
+    )
+    SpaceDp(4)
+    val context = LocalContext.current
+    Text(
+        text = url,
+        style = TextStyle(
+            fontSize = 16.sp,
+            textDecoration = TextDecoration.Underline,
+            color = Color(color = 0xFF0645AD),
+            textAlign = TextAlign.Start
+        ),
+        modifier = Modifier.clickable {
+            CustomTabsIntent.Builder().build().launchUrl(context, Uri.parse(url))
+        }
     )
 }
 
 @Composable
 fun EventDetails() {
     EventDetailsItem(stringResource(R.string.location), "C. Jordi Girona, 12")
-    Spacer(modifier = Modifier.padding(13.dp))
+    SpaceDp(12)
     val strStartTime = "8:00"
     val strEndTime = "18:00"
     val strTimeInfo = "De $strStartTime a $strEndTime"
     EventDetailsItem(stringResource(R.string.time), strTimeInfo)
-    Spacer(modifier = Modifier.padding(13.dp))
+    SpaceDp(12)
     EventDetailsItem(stringResource(R.string.description), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in imperdiet dui, ut viverra ante. Morbi in eros tincidunt, convallis lorem in, commodo mauris. In ultricies turpis vel suscipit suscipit. Vestibulum a lectus nisl. Quisque viverra dolor eu nunc elementum consequat. Phasellus sed tempus velit. Vivamus et viverra enim. Proin tempus odio nec quam elementum, vel rutrum felis volutpat. Aenean dapibus consectetur luctus. Duis lacinia sem quis est venenatis, at mollis felis scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in imperdiet dui, ut viverra ante. Morbi in eros tincidunt, convallis lorem in, commodo mauris. In ultricies turpis vel suscipit suscipit. Vestibulum a lectus nisl. Quisque viverra dolor eu nunc elementum consequat. Phasellus sed tempus velit. Vivamus et viverra enim. Proin tempus odio nec quam elementum, vel rutrum felis volutpat. Aenean dapibus consectetur luctus. Duis lacinia sem quis est venenatis, at mollis felis scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in imperdiet dui, ut viverra ante. Morbi in eros tincidunt, convallis lorem in, commodo mauris. In ultricies turpis vel suscipit suscipit. Vestibulum a lectus nisl. Quisque viverra dolor eu nunc elementum consequat. Phasellus sed tempus velit. Vivamus et viverra enim. Proin tempus odio nec quam elementum, vel rutrum felis volutpat. Aenean dapibus consectetur luctus. Duis lacinia sem quis est venenatis, at mollis felis scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in imperdiet dui, ut viverra ante. Morbi in eros tincidunt, convallis lorem in, commodo mauris. In ultricies turpis vel suscipit suscipit. Vestibulum a lectus nisl. Quisque viverra dolor eu nunc elementum consequat. Phasellus sed tempus velit. Vivamus et viverra enim. Proin tempus odio nec quam elementum, vel rutrum felis volutpat. Aenean dapibus consectetur luctus. Duis lacinia sem quis est venenatis, at mollis felis scelerisque.")
+    SpaceDp(12)
+    EventDetailsLinkItem(stringResource(R.string.link), "https://www.youtube.com/watch?v=Hvtc9BWahMQ")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MeetCatUITheme {
-        IconButton(
-            onClick = {},
-            modifier = Modifier
-                .alpha(1.0f)
-                .padding(start = 16.dp, top = 16.dp)
-                .clip(CircleShape)
-                .border(1.dp, Color(0xFFA0A0A0), shape = CircleShape)
-                .background(color = Color(color = 0xFFF4F4F4)),
-
-            ) {
-
-            Icon(
-                imageVector = Icons.Filled.ArrowBack,
-                contentDescription = null,
-                modifier = Modifier.size(32.dp),
-                tint = Color(0xFF0E0E0E),
-            )
-        }
+        BackButton()
         Column (
             modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-
-            Spacer(modifier = Modifier.padding(16.dp))
-            EventTitle("PES")
-            Spacer(modifier = Modifier.padding(8.dp))
-            EventSubtitle("PES2")
-            Spacer(modifier = Modifier.padding(8.dp))
-            EventDate("13/10/2022", String())
-            Spacer(modifier = Modifier.padding(16.dp))
-            Row(modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp, 8.dp, 8.dp, 16.dp), horizontalArrangement = Arrangement.Start) {
-                Column (Modifier.fillMaxSize()) {
-                    EventDetails()
+                .verticalScroll(rememberScrollState())
+                .padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "MeetCat's Release Party",
+                    style = MaterialTheme.typography.h2
+                )
+                Text(
+                    text = "A party",
+                    style = MaterialTheme.typography.h3
+                )
+                EventDate(startDate = "13/10/2022")
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+                SpaceDp()
+                EventDetails()
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                SpaceDp()
+                Button(onClick = {}) {
+                    Text("Join placeholder")
                 }
             }
         }
-
-
     }
 }
--- app/src/main/java/com/pes/meetcatui/ui/theme/Type.kt
+++ app/src/main/java/com/pes/meetcatui/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
@@ -11,7 +12,23 @@ val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        textAlign = TextAlign.Justify,
+        fontSize = 16.sp,
+    )
+    , h2 = TextStyle(
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp,
+    )
+    , h3 = TextStyle(
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+    )
+    , h4 = TextStyle(
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
     )
     /* Other default text styles to override
     button = TextStyle(

fff8d812c6b2f4389d690fca123796e123f24a8f
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2022-10-19 19:51:14 +0200

    MainActivity fix

--- app/src/main/java/com/pes/meetcatui/MainActivity.kt
+++ app/src/main/java/com/pes/meetcatui/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.pes.meetcatui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -14,6 +16,10 @@ import org.koin.androidx.compose.getViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.pes.meetcatui.feature_event.presentation.EventScreenDestination
 
 
@@ -35,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     App()
-                    Greeting("Android")
+
                     Button(onClick = {mGoogleSignInClient.signOut().addOnCompleteListener {
                         val intent= Intent(this, LoginScreen::class.java)
                         startActivity(intent)

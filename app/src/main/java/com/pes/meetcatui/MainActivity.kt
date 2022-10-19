bf1d6c6c2f53ae9ecceaadc430417f8eeb126631
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2022-10-19 19:27:32 +0200

    google login/register

--- app/src/main/java/com/pes/meetcatui/MainActivity.kt
+++ app/src/main/java/com/pes/meetcatui/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.pes.meetcatui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.pes.meetcatui.ui.theme.MeetCatUITheme
 
 class MainActivity : ComponentActivity() {
+
+    // declare the GoogleSignInClient
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,9 +37,22 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                    Button(onClick = {mGoogleSignInClient.signOut().addOnCompleteListener {
+                        val intent= Intent(this, LoginScreen::class.java)
+                        startActivity(intent)
+                        finish()
+                    }}) {
+
+                    }
                 }
             }
         }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
+        
     }
 }
 

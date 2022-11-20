ffd87552f6745c337335af82386f36b3d43968ba
Yavanna92 <txellguevara.1905@gmail.com>
2022-11-20 21:22:31 +0100

    Load events on scroll

--- app/src/main/java/com/pes/meetcatui/feature_event/presentation/EventListScreenState.kt
+++ app/src/main/java/com/pes/meetcatui/feature_event/presentation/EventListScreenState.kt
@@ -3,7 +3,7 @@ package com.pes.meetcatui.feature_event.presentation
 import com.pes.meetcatui.feature_event.domain.Event
 
 data class EventListScreenState(
-    val data: List<Event>? = null,
+    val data: MutableList<Event>? = null,
     val isLoading: Boolean = false,
     val hasError: Boolean = false,
     val errorMessage: String? = null,

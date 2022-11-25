da049c479b60f6e0aee7c1a21771e2d4a58b1da9
darko.iglesias <darko.iglesias@estudiantat.upc.edu>
2022-11-25 18:50:36 +0100

    Events in map should work not tested cuz my simulator dont want to

--- app/src/main/java/com/pes/meetcatui/feature_event/presentation/MapView.kt
+++ app/src/main/java/com/pes/meetcatui/feature_event/presentation/MapView.kt
@@ -4,26 +4,35 @@ package com.pes.meetcatui.feature_event.presentation
 import android.Manifest
 import android.location.Location
 import android.os.Looper
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.pes.meetcatui.R
 import com.google.accompanist.permissions.*
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
+
+import com.pes.meetcatui.feature_event.domain.Event
 import com.pes.meetcatui.ui.theme.Background
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 const val MapScreenDestination = "Map"
+private const val INITIAL_LATITUDE = 41.387423
+private const val INITIAL_LONGITUDE = 2.169763
+private const val INITIAL_ZOOM = 16f
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -32,14 +41,24 @@ fun MapScreen(
     navToEventList: () -> Unit,
     fusedLocationClient: FusedLocationProviderClient,
 ) {
-    val permissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
 
+    val permissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
+    val events by viewModel.events
+    val selectedEvent by viewModel.selectedEvent
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Background,
-        ) {
+    ) {
         if (permissionState.status.isGranted) {
-            displayMap(fusedLocationClient, viewModel)
+            displayMap(
+                fusedLocationClient = fusedLocationClient,
+                viewModel = viewModel,
+                events = events,
+                selectedEvent = selectedEvent,
+                onEventSelectedId = {
+                    viewModel.onEventSelectId(it)
+                },
+            )
         } else {
             permissionNotGranted(permissionState)
         }
@@ -53,14 +72,23 @@ fun MapScreen(
 @Composable
 fun displayMap(
     fusedLocationClient: FusedLocationProviderClient,
+    selectedEvent: Event,
     viewModel: MapViewModel,
+    onEventSelectedId: (Long?) -> Unit,
+    events: EventListScreenState,
 ) {
     val mapState by viewModel.mapState
-
+    var cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(
+            LatLng(INITIAL_LATITUDE, INITIAL_LONGITUDE),
+            INITIAL_ZOOM,
+        )
+    }
+    val scope = rememberCoroutineScope()
     Text("Permission is granted")
 
     fusedLocationClient.lastLocation
-        .addOnSuccessListener { location : Location? ->
+        .addOnSuccessListener { location: Location? ->
             if (location != null)
                 viewModel.setPosition(location)
         }
@@ -72,18 +100,76 @@ fun displayMap(
             Looper.getMainLooper()
         )
 
-    if (mapState.lastLocation != null) {
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(mapState.gpsCoords, 10f)
+    Column {
+        Map(
+            modifier = Modifier.weight(1f).fillMaxWidth(),
+            mapState,
+            cameraPositionState,
+            onEventClicked = {
+                viewModel.onEventSelectId(it.eventId)
             },
+            onOutsideClicked = {
+                onEventSelectedId(null)
+            },
+            events = events,
+        )
+        AnimatedVisibility(visible = selectedEvent != null) {
+            if (selectedEvent != null) {
+                EventDisplay(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .animateContentSize()
+                        .padding(16.dp),
+                    event = selectedEvent,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun Map(
+    modifier: Modifier = Modifier,
+    mapState: MapScreenState,
+    cameraPositionState: CameraPositionState,
+    onOutsideClicked: () -> Unit,
+    events: EventListScreenState,
+    onEventClicked: (Event) -> Unit
+) {
+    var cameraPositionState1 = cameraPositionState
+    if (mapState.lastLocation != null) {
+        cameraPositionState1 = rememberCameraPositionState {
+            position =
+                CameraPosition.fromLatLngZoom(mapState.gpsCoords, INITIAL_ZOOM)
+        }
+    }
+    val scope = rememberCoroutineScope()
+    Box(modifier = modifier) {
+        GoogleMap(
+            cameraPositionState = cameraPositionState1,
+            onMapClick = { onOutsideClicked() },
         ) {
-            Marker(
-                state = MarkerState(position = mapState.gpsCoords),
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
+            if (events.data != null) {
+                events.data.forEach { event ->
+                    if (event.location != null) {
+                        val ubi = event.location
+                        val ll = ubi.split(',')
+                        Marker(
+                            state = MarkerState(
+                                position = LatLng(ll[0].toDouble(), ll[1].toDouble())
+                            ),
+                            onClick = {
+                                scope.launch {
+                                    cameraPositionState1.animate(CameraUpdateFactory.newLatLng(it.position))
+                                }
+                                onEventClicked(event)
+                                true
+                            },
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -107,6 +193,11 @@ fun permissionNotGranted(permissionState: PermissionState) {
             Text(stringResource(R.string.permission))
         }
     }
+}
+
+@Composable
+fun EventDisplay(modifier: Modifier, event: Event) {
+    Text(event.name)
 }
 
 
--- app/src/main/java/com/pes/meetcatui/feature_event/presentation/MapViewModel.kt
+++ app/src/main/java/com/pes/meetcatui/feature_event/presentation/MapViewModel.kt
@@ -2,24 +2,31 @@ package com.pes.meetcatui.feature_event.presentation
 
 
 import android.location.Location
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.model.LatLng
+import com.pes.meetcatui.feature_event.Resource
 import com.pes.meetcatui.feature_event.domain.DataRepository
+import com.pes.meetcatui.feature_event.domain.Event
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class MapViewModel(
     val dataRepository: DataRepository,
 ) : ViewModel() {
 
+    val events = mutableStateOf(EventListScreenState())
     val mapState = mutableStateOf(MapScreenState())
+    var selectedEvent = mutableStateOf(Event(0,"",null,null,"",null,null,null,null,null))
     private val locationRequest = LocationRequest
         .Builder(120000)
         .build()
-
     fun getLocationCallback() : LocationCallback {
         return object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
@@ -43,5 +50,38 @@ class MapViewModel(
             lastLocation = mapState.value.lastLocation,
             gpsCoords = LatLng(location.latitude, location.longitude),
         )
+    }
+
+    fun onEventSelectId(eventId: Long?){
+        events.value.data?.forEach { event ->
+            if (event.eventId == eventId){
+                selectedEvent.value = event
+            }
+        }
+    }
+
+    init {
+        viewModelScope.launch {
+            dataRepository.getEvents(0).collect { resource ->
+                when (resource) {
+                    is Resource.Success -> {
+                        events.value = EventListScreenState(
+                            data = resource.data?.events as MutableList<Event>
+                        )
+                    }
+                    is Resource.Error -> {
+                        events.value = EventListScreenState(
+                            hasError = true,
+                            errorMessage = resource.message
+                        )
+                    }
+                    is Resource.Loading -> {
+                        events.value = EventListScreenState(
+                            isLoading = true
+                        )
+                    }
+                }
+            }
+        }
     }
 }
